48cda988dc1ca3c14d38bbb242c9966df55a3d09
chenjing <100081662+chenjingdev@users.noreply.github.com>
2024-03-27 14:38:09 +0900

    [Silver I] Title: 하노이 탑, Time: 856 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,18 +1,15 @@
-#하노이 함수
-def hanoi_f(start, end, n):
+def hanoi(n, start, end, aux):
     if n == 1:
-        print(start,end)
+        print(start, end)
         return
+    # n-1개를 aux로 이동
+    hanoi(n-1, start, aux, end)
+    print(start, end)
+    # n-1개를 end로 이동
+    hanoi(n-1, aux, end, start)
 
-    hanoi_f(start, 6-start-end, n-1) #1단계 (1->2)
-    print(start, end) #2단계 (마지막원반 1->3)
-    hanoi_f(6-start-end, end, n-1) #3단계 (2->3)
-
-#메인
 n = int(input())
-print(2**n-1)
+print(2**n - 1)
 
 if n <= 20:
-    hanoi_f(1,3,n)
-
-    
+    hanoi(n, 1, 3, 2)
