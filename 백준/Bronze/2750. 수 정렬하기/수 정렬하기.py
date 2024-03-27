6cc6702528b03298aca89da2207b46da8f449e89
chenjing <100081662+chenjingdev@users.noreply.github.com>
2024-03-27 15:08:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,6 +1,60 @@
-n = int(input())
-numbers = [int(input()) for _ in range(n)]
-numbers.sort()
+MIN_RUN = 32
 
-for num in numbers:
+def insertion_sort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= left and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def merge(arr, l, m, r):
+    len1, len2 = m - l + 1, r - m
+    left, right = [], []
+    for i in range(len1):
+        left.append(arr[l + i])
+    for i in range(len2):
+        right.append(arr[m + 1 + i])
+
+    i = j = 0
+    k = l
+
+    while i < len1 and j < len2:
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len1:
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len2:
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+def tim_sort(arr):
+    n = len(arr)
+    for start in range(0, n, MIN_RUN):
+        end = min(start + MIN_RUN - 1, n - 1)
+        insertion_sort(arr, start, end)
+
+    size = MIN_RUN
+    while size < n:
+        for left in range(0, n, 2 * size):
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), (n - 1))
+            if mid < right:
+                merge(arr, left, mid, right)
+        size *= 2
+
+arr = [int(input()) for _ in range(int(input()))]
+tim_sort(arr)
+for num in arr:
     print(num)
